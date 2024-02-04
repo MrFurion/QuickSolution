ba12d052f316a38684009521060eddb69e05af14
MrFurion <139688927+MrFurion@users.noreply.github.com>
2024-02-04 18:54:59 +0300

    added @Nullable

--- src/main/java/by/trubetski/QuickSolution/config/PasswordEncoderExample.java
+++ src/main/java/by/trubetski/QuickSolution/config/PasswordEncoderExample.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class PasswordEncoderExample {
     public static void main(String[] args) {
-        String password = "4444";
+        String password = "123123";
 
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
         String hashedPassword = passwordEncoder.encode(password);
